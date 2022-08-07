2a56c4fcb86f2d108f37e337dd8043acade030e6
César Aparicio <clark012389@hotmail.com>
2022-08-06 19:56:12 -0500

    Test de destructuracion de arreglo, verificando el tipo de dato retornado

---
+++ tests/base-pruebas/07-deses-arr.test.js
@@ -0,0 +1,13 @@
+import { retornaArreglo } from "../../src/base-pruebas/07-deses-arr";
+
+describe("Pruebas en 07-deses-arr", () => {
+  test("retornaArreglo() deberia retornar un elemento tipo string, un tipo numbero y un booleano", () => {
+    const [
+      elementoString, elementoNumero, elementoBooleano
+    ] = retornaArreglo();
+
+    expect(typeof elementoString).toBe("string");
+    expect(typeof elementoNumero).toBe("number");
+    expect(typeof elementoBooleano).toBe("boolean");
+  });
+});
